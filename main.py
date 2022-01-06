cac25595bd9d860266cc7518d6329f315e6af768
Matyáš Kříž <kriz.matty@gmail.com>
2022-01-06 18:54:35 +0100

    Use CSV as JSON has been discontinued.

--- main.py
+++ main.py
@@ -6,33 +6,26 @@ import os
 import sys
 import re
 import datetime
+import csv
 
 slack_url = os.getenv('SLACK_URL')
 if not slack_url:
     print('Provide a hook to your Slack workspace in the `SLACK_URL` environment property.\n(e.g. https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX)')
     sys.exit('Slack hook URL is necessary to post the overview into the workspace.')
 
-approximate_citizen_count = 10_707_839
+approximate_citizen_count = 10_701_777
 
 mzcr_url = 'https://onemocneni-aktualne.mzcr.cz/covid-19'
 
-tests_url = 'https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/testy-pcr-antigenni.json'
-overview_url = 'https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/zakladni-prehled.json'
-vaccinations_url = 'https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovani.json'
+overview_url = 'https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/zakladni-prehled.csv'
 
-tests_json = requests.get(tests_url).json()
-overview_json = requests.get(overview_url).json()
-vaccinations_json = requests.get(vaccinations_url).json()
+overview_csv = next(csv.DictReader(requests.get(overview_url).content.decode('utf-8-sig').splitlines()))
 
-# TODO: Post detailed info about vaccine distribution and tests with and without symptoms into message's thread.
-tests_pcr = sum(int(data_point['pocet_PCR_testy']) for data_point in tests_json['data'])
-tests_antigen = sum(int(data_point['pocet_AG_testy']) for data_point in tests_json['data'])
+tests_antigen = overview_csv['provedene_antigenni_testy_celkem']
+tests_pcr = int(overview_csv['provedene_testy_celkem']) - int(overview_csv['provedene_antigenni_testy_celkem'])
 
-vaccines_by_name = { data_point['vakcina'] for data_point in vaccinations_json['data'] }
-total_first_dose_vaccinations = sum(int(data_point['prvnich_davek']) for data_point in vaccinations_json['data'])
-total_first_dose_vaccinations_percentage = '{:.1%}'.format(total_first_dose_vaccinations / approximate_citizen_count)
-total_second_dose_vaccinations = sum(int(data_point['druhych_davek']) for data_point in vaccinations_json['data'])
-total_second_dose_vaccinations_percentage = '{:.1%}'.format(total_second_dose_vaccinations / approximate_citizen_count)
+total_vaccinations = int(overview_csv['vykazana_ockovani_celkem'])
+total_vaccinations_percentage = '{:.1%}'.format(total_vaccinations / approximate_citizen_count)
 
 # We're rewriting that file. If it's not ours, it has no business being here.
 dirname = os.path.dirname(__file__)
@@ -45,13 +38,12 @@ with open(previous_data_path, 'r+') as f:
     current_data_points = [
         ('tests_pcr', tests_pcr),
         ('tests_antigen', tests_antigen),
-        ('total_infections', overview_json['data'][0]['potvrzene_pripady_celkem']),
-        ('active_infections', overview_json['data'][0]['aktivni_pripady']),
-        ('recoveries', overview_json['data'][0]['vyleceni']),
-        ('hospitalized', overview_json['data'][0]['aktualne_hospitalizovani']),
-        ('deceased', overview_json['data'][0]['umrti']),
-        ('vaccinations_first_dose', total_first_dose_vaccinations),
-        ('vaccinations_second_dose', total_second_dose_vaccinations),
+        ('total_infections', overview_csv['potvrzene_pripady_celkem']),
+        ('active_infections', overview_csv['aktivni_pripady']),
+        ('recoveries', overview_csv['vyleceni']),
+        ('hospitalized', overview_csv['aktualne_hospitalizovani']),
+        ('deceased', overview_csv['umrti']),
+        ('total_vaccinations', total_vaccinations),
     ]
     current_data = {key: int(value) for key, value in current_data_points}
     f.seek(0)
@@ -79,16 +71,6 @@ def format_comparison(old_value, new_value):
         )
     else:
         return ''
-
-def format_modified(date):
-    return date.strftime("%d/%m/%Y @ %H:%M")
-
-def modified(data):
-    return datetime.datetime.strptime(data['modified'], '%Y-%m-%dT%H:%M:%S%z')
-
-tests_modified = modified(tests_json)
-overview_modified = modified(overview_json)
-vaccinations_modified = modified(vaccinations_json)
 
 previous_total_tests = None
 
@@ -187,7 +169,6 @@ payload = {
                 format_comparison(previous_total_tests, current_data['tests_pcr'] + current_data['tests_antigen']),
             )
         ),
-        Slack.context("Last update: *{}*".format(format_modified(tests_modified))),
         Slack.divider(),
         Slack.section(
             "[Infections] :zombie: :female_zombie:" +
@@ -204,7 +185,6 @@ payload = {
                 format_comparison(previous_data.get('total_infections'), current_data['total_infections']),
             )
         ),
-        Slack.context("Last update: *{}*".format(format_modified(overview_modified))),
         Slack.divider(),
         Slack.section(
             "[Losses] :hospital: :f:" +
@@ -217,22 +197,14 @@ payload = {
                 format_comparison(previous_data.get('deceased'), current_data['deceased']),
             )
         ),
-        Slack.context("Last update: *{}*".format(format_modified(overview_modified))),
         Slack.divider(),
         Slack.section(
             "[Vaccinations] :syringe: :pill:" +
-            "\n\t First dose: *{}* = {}{}".format(
-                format_number(current_data['vaccinations_first_dose']),
-                total_first_dose_vaccinations_percentage,
-                format_comparison(previous_data.get('vaccinations_first_dose'), current_data['vaccinations_first_dose']),
-            ) +
-            "\n\t Second dose: *{}* = {}{}".format(
-                format_number(current_data['vaccinations_second_dose']),
-                total_second_dose_vaccinations_percentage,
-                format_comparison(previous_data.get('vaccinations_second_dose'), current_data['vaccinations_second_dose']),
+            "\n\t Any dose: *{}*{}".format(
+                format_number(current_data['total_vaccinations']),
+                format_comparison(previous_data.get('total_vaccinations'), current_data['total_vaccinations']),
             )
         ),
-        Slack.context("Last update: *{}*".format(format_modified(vaccinations_modified))),
         Slack.divider(),
         {
             "type": "section",
